67b27a95160e5023cca85ac251805513b494585b
emmoistner <emmoistner@gmail.com>
2019-09-05 00:08:54 -0400

    [refactor] upload button to separate module

--- frontend/src/modules/home/index.js
+++ frontend/src/modules/home/index.js
@@ -1,78 +1,16 @@
 import React, { PureComponent } from 'react'
-import Snackbar from '../../components/snackbar'
-import Button from '../../components/button'
 
 import './home.css'
 import Logo from '../../components/logo'
 
-import { createFile } from '../../api/file'
+import Upload from '../upload'
 
 export default class Home extends PureComponent {
-  constructor(props) {
-    super(props)
-
-    this.uploadFile = this.uploadFile.bind(this)
-    this.onSnackbarClose = this.onSnackbarClose.bind(this)
-
-    this.state = {
-      isSnackbarOpen: false
-    }
-  }
-
-  uploadFile(e) {
-    const file = e.target.files[0]
-    createFile(file)
-      .then((response) => {
-        if (response && response.status === 200) {
-          this.setState({ isSnackbarOpen: true, fileName: response.data.name })
-        }
-      })
-  }
-
-  onSnackbarClose(reason) {
-    if (reason === 'clickaway') {
-      return
-    }
-
-    this.setState({ isSnackbarOpen: false })
-  }
-
   render() {
     return (
       <div className='home'>
         <Logo className='logo' />
-        <input
-          accept='*'
-          style={{ display: 'none' }}
-          id='outlined-button-file'
-          type='file'
-          onChange={this.uploadFile}
-        />
-        <label htmlFor='outlined-button-file'>
-          <Button
-            className='upload-button'
-            variant='outlined'
-            size='large'
-            color='primary'
-            component='span'
-          >
-            Upload File
-          </Button>
-        </label>
-        <Snackbar
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left'
-          }}
-          open={this.state.isSnackbarOpen}
-          autoHideDuration={6000}
-          onClose={this.onSnackbarClose}
-          variant='success'
-          message={`
-            ${this.state.fileName ? this.state.fileName : 'File'}
-            uploaded
-          `}
-        />
+        <Upload />
       </div>
     )
   }
