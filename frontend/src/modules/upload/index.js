39be72106ed0338b8c2252667854877ab1e0703a
emmoistner <emmoistner@gmail.com>
2019-09-05 00:24:00 -0400

    [refactor] upload class to function and hooks

--- frontend/src/modules/upload/index.js
+++ frontend/src/modules/upload/index.js
@@ -1,79 +1,70 @@
-import React, { PureComponent } from 'react'
-import Snackbar from '../../components/snackbar'
+import React, { useState } from 'react'
 
+import Snackbar from '../../components/snackbar'
 import Button from '../../components/button'
 
 import { createFile } from '../../api/file'
 
-export default class Upload extends PureComponent {
-  constructor(props) {
-    super(props)
 
-    this.uploadFile = this.uploadFile.bind(this)
-    this.onSnackbarClose = this.onSnackbarClose.bind(this)
+export default function Upload() {
+  const [isSnackbarOpen, setIsSnackbarOpen] = useState(false)
+  const [fileName, setFileName] = useState()
 
-    this.state = {
-      isSnackbarOpen: false
-    }
-  }
-
-  uploadFile(e) {
+  function uploadFile(e) {
     const file = e.target.files[0]
     createFile(file)
       .then((response) => {
         if (response && response.status === 200) {
-          this.setState({ isSnackbarOpen: true, fileName: response.data.name })
+          setIsSnackbarOpen(true)
+          setFileName(response.data.name)
         }
       })
   }
 
-  onSnackbarClose(reason) {
+  function onSnackbarClose(reason) {
     if (reason === 'clickaway') {
       return
     }
 
-    this.setState({ isSnackbarOpen: false })
+    setIsSnackbarOpen(false)
   }
 
-  render() {
-    return (
-      <>
-        <input
-          accept='*'
-          style={{ display: 'none' }}
-          id='outlined-button-file'
-          type='file'
-          onChange={this.uploadFile}
-        />
-        <label htmlFor='outlined-button-file'>
-          <Button
-            className='upload-button'
-            variant='outlined'
-            size='large'
-            color='primary'
-            component='span'
-          >
-            Upload File
-          </Button>
-        </label>
-        <Snackbar
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left'
-          }}
-          open={this.state.isSnackbarOpen}
-          autoHideDuration={6000}
-          onClose={this.onSnackbarClose}
-          variant='success'
-          message={`
-            ${this.state.fileName ? this.state.fileName : 'File'}
-            uploaded
-          `}
-        />
-      </>
-    )
-  }
+  return (
+    <>
+      <input
+        accept='*'
+        style={{ display: 'none' }}
+        id='outlined-button-file'
+        type='file'
+        onChange={uploadFile}
+      />
+      <label htmlFor='outlined-button-file'>
+        <Button
+          className='upload-button'
+          variant='outlined'
+          size='large'
+          color='primary'
+          component='span'
+        >
+          Upload File
+        </Button>
+      </label>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left'
+        }}
+        open={isSnackbarOpen}
+        autoHideDuration={6000}
+        onClose={onSnackbarClose}
+        variant='success'
+        message={`
+          ${fileName ? fileName : 'File'}
+          uploaded
+        `}
+      />
+    </>
+  )
 }
 
 Upload.propTypes = {}
-Upload.defaultProps = {}
